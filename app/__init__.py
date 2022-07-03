a5e8cf8b4463c25cbb82a2173c3fbac1d89551b4
Kiana <kianashui@gmail.com>
2022-07-02 20:36:32 -0700

    added mongodb connection and URI environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+import pymongo
+import dns #needed to use mongodb+srv:// URIs
+# from pymongo import MongoClient
+
+load_dotenv()
+client = pymongo.MongoClient(os.environ.get("MONGODB_CONNECTION_STRING"))
+db = client.test
 
 def create_app(test_config=None):
     app = Flask(__name__)
--- app/routes.py
+++ app/routes.py
@@ -46,55 +46,6 @@ trips = [
 
 trip_bp = Blueprint("trip_bp", __name__, url_prefix="/trips")
 
-# trips = [
-#     {
-#         "name": "San Francisco Road Trip",
-#         "start_date": "2022-08-19",
-#         "end_date": "2022-08-28",
-#         "itinerary_entries": [
-#             {
-#                 "activity_type": "Food",
-#                 "name": "Bouchon Bistro",
-#                 "location": "Napa, CA",
-#                 "price": 50.50,
-#                 "start_time": "1:00 PM",
-#                 "end_time": "2:00 PM"
-#             },
-#             {
-#                 "activity_type": "Accommodations",
-#                 "name": "Andaz Hyatt Napa",
-#                 "location": "Napa, CA",
-#                 "price": 0,
-#                 "start_time": "2:00 PM",
-#                 "end_time": "3:00 PM"
-#             }
-#         ]
-#     },
-#     {
-#         "name": "Chile",
-#         "start_date": "2023-01-28",
-#         "end_date": "2023-02-12",
-#         "itinerary_entries": [
-#             {
-#                 "activity_type": "Flight",
-#                 "name": "LAX-LIM flight",
-#                 "location": "Los Angeles, CA",
-#                 "price": 105,
-#                 "start_time": "8:00 PM",
-#                 "end_time": "9:00 PM"
-#             },
-#             {
-#                 "activity_type": "Flight",
-#                 "name": "LIM-SCL flight",
-#                 "location": "Lima, Peru",
-#                 "price": 0,
-#                 "start_time": "6:00 AM",
-#                 "end_time": "9:00 AM"
-#             }
-#         ]
-#     }
-# ]
-
 def validate_id(id):
     try:
         id = int(id)
@@ -104,8 +55,6 @@ def validate_id(id):
     return id
 
 # def retrieve_object(id, Model):
-
-
 
 @trip_bp.route("", methods=["GET"])
 def get_trips():
